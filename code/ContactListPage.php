10cb3411ddbc85d7296925b10eb9be79e1950fbd
Nicolaas Thiemen Francken @ B&C <github@sunnysideup.co.nz>
2017-02-09 18:22:37 +1300

    PATCH: better TableFilterSort

--- code/ContactListPage.php
+++ code/ContactListPage.php
@@ -77,7 +77,13 @@ class ContactListPage_Controller extends Page_Controller
     public function init()
     {
         parent::init();
-        TableFilterSortAPI::include_requirements();
+        TableFilterSortAPI::include_requirements(
+            $tableSelector = '.tfs-holder',
+            $blockArray = array('TableFilterSort.theme'),
+            $jqueryLocation = '',
+            $includeInPage = true,
+            $jsSettings = '{serverConnectionURL: ""}'
+        );
     }
 
     public function Contacts()
